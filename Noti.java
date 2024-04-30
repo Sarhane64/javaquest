235fb20fd97593a84e473c33e57ea99cfb98eccc
theo sarhane <theosarhane@MacBook-Pro-de-theo.local>
2024-04-30 11:55:37 +0200

    commit

--- Noti.java
+++ Noti.java
@@ -1,9 +1,22 @@
 class Notice {
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("C'est " + args[0] + " developeur java");
+        String[][] filmsActeurs = {
+            {"Kingdom of the Crystal Skull", "Harrison Ford", "Cate Blanchett", "Shia LaBeouf"},
+            {"The Last Crusade", "Harrison Ford", "Sean Connery", "Denholm Elliott"},
+            {"The Temple of Doom", "Harrison Ford", "Kate Capshaw", "Jonathan Ke Quan"}
+        };
+
+        // Boucle pour afficher les films et leurs acteurs associés
+        for (String[] filmActeurs : filmsActeurs) {
+            String film = filmActeurs[0];
+            String acteur1 = filmActeurs[1];
+            String acteur2 = filmActeurs[2];
+            String acteur3 = filmActeurs[3];
+            System.out.println("Dans le film " + film + ", les principaux acteurs sont : " + acteur1 + ", " + acteur2 + ", " + acteur3);
+        }
     }
 }
+
 
 
 
